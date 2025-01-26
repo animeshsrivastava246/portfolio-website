a8e4d7f5a146d32193a25141843f03d003d60ffd
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-01-26 23:45:21 +0530

    Added: Started styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,42 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<header>
-			<div>
-				<Link to="/">
+		<header className="bg-gray-800 text-white shadow-md sticky top-0 z-50">
+			<div className="container mx-auto flex justify-between items-center py-4 px-6">
+				{/* Logo Section */}
+				<Link
+					to="/"
+					className="text-2xl font-bold hover:text-blue-400 transition duration-200"
+				>
 					My Portfolio
 				</Link>
+
+				{/* Navigation Menu */}
 				<nav>
-					<ul>
+					<ul className="flex space-x-6 text-lg">
 						<li>
-							<Link to="/about">About</Link>
+							<Link
+								to="/about"
+								className="hover:text-blue-400 transition duration-200"
+							>
+								About
+							</Link>
 						</li>
 						<li>
-							<Link to="/projects">Projects</Link>
+							<Link
+								to="/projects"
+								className="hover:text-blue-400 transition duration-200"
+							>
+								Projects
+							</Link>
 						</li>
 						<li>
-							<Link to="/contact">Contact</Link>
+							<Link
+								to="/contact"
+								className="hover:text-blue-400 transition duration-200"
+							>
+								Contact
+							</Link>
 						</li>
 					</ul>
 				</nav>
