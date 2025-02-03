a3636e2d16a9437866ae56172b5981d7b7a2693f
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-02-03 20:51:55 +0530

    Added: Fixed Not Found Page.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
 	return (
 		<section className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center px-6">
-			<h1 className="text-7xl font-extrabold text-red-800">404</h1>
-			<h2 className="text-3xl font-semibold text-gray-800 mt-4">
+			<h1 className="text-7xl font-extrabold text-red-500">404</h1>
+			<h2 className="text-5xl font-semibold text-gray-800 mt-4">
 				Oops! Page Not Found
 			</h2>
 			<p className="text-gray-600 mt-2 max-w-lg">
