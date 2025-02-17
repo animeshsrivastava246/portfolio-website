9f5b8c30c5832e601492a73aaa47f7ce7bdd75b0
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-02-17 21:02:39 +0530

    Changed: Working on nav, configured Tailwind

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,112 +1,56 @@
 <script lang="ts">
-	let isOpen = false;
-
+	const menuItems = [
+		{ name: "About", id: "About" },
+		{ name: "Works", id: "WorkExperience" },
+		{ name: "Projects", id: "Projects" },
+		{ name: "Contact", id: "Contact" },
+	];
+  
 	const scrollToSection = (id: string) => {
-		const section = document.getElementById(id);
-		if (section) {
+	  	const section = document.getElementById(id);
+	  	if (section) {
 			section.scrollIntoView({ behavior: "smooth" });
-			isOpen = false; // Close mobile menu on click
 		}
 	};
 </script>
-
+  
 <nav
-	class="fixed top-4 left-1/2 transform -translate-x-1/2 max-w-3xl md:w-full bg-white/10 backdrop-blur-lg shadow-lg border-2 border-white/30 rounded-[2em] z-50 transition-all duration-300"
+	class="fixed top-4 left-1/2 transform -translate-x-1/2 max-w-3xl w-full 
+	bg-white/10 backdrop-blur-lg shadow-lg border-2 border-white/30 
+	rounded-[2em] z-50 transition-all duration-300"
 >
 	<div class="mx-auto px-3 flex justify-between items-center h-16">
-		<!-- Logo -->
-		<button
+	  	<!-- Logo -->
+	  	<button
 			on:click={() => scrollToSection("Hero")}
-			class="text-xl font-bold text-white tracking-wide hover:text-accent transition-all whitespace-nowrap"
+			class="text-xl cursor-pointer font-bold text-white tracking-wide 
+			hover:text-accent transition-all whitespace-nowrap"
 		>
 			&lt; DevPortfolio /&gt;
-		</button>
-
-		<!-- Desktop Navigation -->
-		<ul class="hidden md:flex space-x-6 mx-auto">
-			<li>
+	  	</button>
+  
+	  	<!-- Desktop Navigation -->
+	 	<ul class="flex space-x-6 mx-auto">
+			{#each menuItems as item}
+		  	<li>
 				<button
-					on:click={() => scrollToSection("About")}
-					class="nav-btn text-xl">About</button
+					on:click={() => scrollToSection(item.id)}
+					class="text-xl hover:text-black rounded-full cursor-pointer 
+					transition-all duration-300"
 				>
+					{item.name}
+				</button>
 			</li>
-			<li>
-				<button
-					on:click={() => scrollToSection("WorkExperience")}
-					class="nav-btn text-xl">Works</button
-				>
-			</li>
-			<li>
-				<button
-					on:click={() => scrollToSection("Projects")}
-					class="nav-btn text-xl">Projects</button
-				>
-			</li>
-			<li>
-				<button
-					on:click={() => scrollToSection("Contact")}
-					class="nav-btn text-xl">Contact</button
-				>
-			</li>
-		</ul>
-
+			{/each}
+	  	</ul>
 		<!-- Resume Button -->
-		<a
+	  	<a
 			href="/resume.pdf"
 			target="_blank"
-			class="hidden md:block text-xl px-5 py-2 border-2 border-white/50 text-white font-semibold rounded-full hover:bg-white hover:text-black transition-all hover:scale-105"
-		>
-			Resume
-		</a>
-
-		<!-- Mobile Menu Button -->
-		<button
-			class="md:hidden text-white focus:outline-none"
-			on:click={() => (isOpen = !isOpen)}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-6 w-6"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" />
-				{#if isOpen}
-					<path d="M6 18L18 6M6 6l12 12" />
-				{:else}
-					<path d="M4 6h16M4 12h16M4 18h16" />
-				{/if}
-			</svg>
-		</button>
+			class="text-xl px-5 py-2 border-2 border-white/50 text-white font-semibold 
+			rounded-full hover:bg-white hover:text-black hover:scale-95 transition-all duration-300"
+	  	>
+			<p>Résumé</p>
+	  	</a>
 	</div>
-
-	<!-- Mobile Menu -->
-	{#if isOpen}
-		<ul class="md:hidden text-white py-4 px-6 space-y-4 text-center rounded-0">
-			<button
-				on:click={() => scrollToSection("About")}
-				class="nav-btn text-xl block w-full">About</button
-			>
-			<button
-				on:click={() => scrollToSection("WorkExperience")}
-				class="nav-btn text-xl block w-full">Works</button
-			>
-			<button
-				on:click={() => scrollToSection("Projects")}
-				class="nav-btn text-xl block w-full">Projects</button
-			>
-			<button
-				on:click={() => scrollToSection("Contact")}
-				class="nav-btn text-xl block w-full">Contact</button
-			>
-			<a
-				href="/resume.pdf"
-				target="_blank"
-				class="block px-5 text-2xl py-2 border-2 border-white/50 text-white font-semibold rounded-full hover:bg-white hover:text-black transition-all hover:scale-105"
-			>
-				Resume
-			</a>
-		</ul>
-	{/if}
-</nav>
+</nav>  
