419744d09f74fdfabe58420b11693c27b57e4b16
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-02-22 21:26:07 +0530

    Changed: Ongoing work in TechStack Section

--- src/sections/TechStack.svelte
+++ src/sections/TechStack.svelte
@@ -1,39 +1,32 @@
 <script lang="ts">
-	let languages = [
-		{ name: "JavaScript", icon: "/icons/javascript.svg" },
-		{ name: "Java", icon: "/icons/java.svg" },
-		{ name: "C++", icon: "/icons/cpp.svg" },
-	];
-
-	let stacks = [
-		{ name: "React.js", icon: "/icons/react.svg" },
-		{ name: "Node.js", icon: "/icons/node.svg" },
-		{ name: "Next.js", icon: "/icons/next.svg" },
-	];
+	// import logos from "/assets/logos/svg";
 </script>
 
-<section id="TechStack" class="py-20 text-center">
-	<h2 class="text-3xl font-bold text-accent">&lt; Languages /&gt;</h2>
-	<!-- <div class="flex flex-wrap justify-center gap-6 mt-6">
-		{#each languages as lang}
-			<div
-				class="bg-white/10 backdrop-blur-md px-6 py-4 rounded-lg flex items-center gap-3 shadow-md"
-			>
-				<img src={lang.icon} alt={lang.name} class="w-8 h-8" />
-				<span class="text-lg">{lang.name}</span>
-			</div>
-		{/each}
+<section id="TechStack" class="my-20 py-20">
+	<h2 class="text-7xl font-bold text-center my-10">&lt; Languages /&gt;</h2>
+	<div class="overflow-hidden">
+		<div class="flex animate-scroll">
+			<!-- {#each logos as logo}
+				<img src={logo} alt="Logo" class="h-40 mx-8" />
+			{/each}
+			{#each logos as logo}
+				<img src={logo} alt="Logo" class="h-40 mx-8" />
+			{/each} -->
+		</div>
 	</div>
-
-	<h2 class="text-3xl font-bold text-accent mt-12">&lt; Stack /&gt;</h2>
-	<div class="flex flex-wrap justify-center gap-6 mt-6">
-		{#each stacks as stack}
-			<div
-				class="bg-white/10 backdrop-blur-md px-6 py-4 rounded-lg flex items-center gap-3 shadow-md"
-			>
-				<img src={stack.icon} alt={stack.name} class="w-8 h-8" />
-				<span class="text-lg">{stack.name}</span>
-			</div>
-		{/each}
-	</div> -->
 </section>
+
+<style>
+	@keyframes scroll {
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(-100%);
+		}
+	}
+
+	.animate-scroll {
+		animation: scroll 35s linear infinite;
+	}
+</style>
