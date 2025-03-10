7c20cce45d5ba3ebf5a88a8cc4ec1ffeb4cae6e3
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-03-10 22:00:57 +0530

    Changing: tilt effects

--- src/utils/tilt.ts
+++ src/utils/tilt.ts
@@ -4,9 +4,9 @@ export const applyTilt = (element: HTMLElement) => {
     if (element) {
         VanillaTilt.init(element, {
             max: 15,      // Max tilt rotation
-            speed: 400,   // Transition speed
+            speed: 300,   // Transition speed
             glare: true,  // Adds reflection glare
-            "max-glare": 0.3, // Glare intensity
+            "max-glare": 0.5, // Glare intensity
         });
     }
 };
